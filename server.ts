1de9ad3a723465090785fa047aee04f5caa6b252
SamOr1014 <orchamsumsam1014@gmail.com>
2022-08-07 00:13:12 +0800

    dashboard nav added

--- server.ts
+++ server.ts
@@ -39,6 +39,7 @@ app.use('/', routes)
 //Static Files
 //########################
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'private')))
 app.use((req, res) => {
 	res.sendFile(path.join(__dirname, 'public', '404.html'))
 })
