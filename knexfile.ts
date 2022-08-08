acaa41ff3210f192e72ea5a6e052d2832859212e
Andy Wong <andyw028@gmail.com>
2022-08-08 18:17:49 +0800

    js

--- knexfile.ts
+++ knexfile.ts
@@ -1,58 +1,47 @@
-import type { Knex } from 'knex'
-import dotenv from 'dotenv'
-dotenv.config()
+import type { Knex } from "knex";
 
 // Update with your config settings.
 
 const config: { [key: string]: Knex.Config } = {
-	development: {
-		client: 'postgresql',
-		debug: true,
-		connection: {
-			database: process.env.DB_NAME,
-			user: process.env.DB_USERNAME,
-			password: process.env.DB_PASSWORD
-		},
-		pool: {
-			min: 2,
-			max: 10
-		},
-		migrations: {
-			tableName: 'knex_migrations'
-		}
-	},
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: "./dev.sqlite3"
+    }
+  },
 
-	staging: {
-		client: 'postgresql',
-		connection: {
-			database: 'my_db',
-			user: 'username',
-			password: 'password'
-		},
-		pool: {
-			min: 2,
-			max: 10
-		},
-		migrations: {
-			tableName: 'knex_migrations'
-		}
-	},
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  },
 
-	production: {
-		client: 'postgresql',
-		connection: {
-			database: 'my_db',
-			user: 'username',
-			password: 'password'
-		},
-		pool: {
-			min: 2,
-			max: 10
-		},
-		migrations: {
-			tableName: 'knex_migrations'
-		}
-	}
-}
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  }
 
-module.exports = config
+};
+
+module.exports = config;
--- services/userServices.ts
+++ services/userServices.ts
@@ -1,9 +1,12 @@
 import { Knex } from 'knex'
+import { userInfo } from 'os'
 
 export class UserServices {
 	constructor(private knex: Knex) {}
 
 	async getUser() {
+		let response = await fetch(userInfo.userInfo)
+		let data = await response.json()
 		return await this.knex('users').select('*')
 	}
 
