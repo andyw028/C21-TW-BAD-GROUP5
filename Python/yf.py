6aa8dbc8a6b8122f977455cb96d94b287600c1c5
SamOr1014 <orchamsumsam1014@gmail.com>
2022-08-10 10:14:17 +0800

    change python file name

--- Python/yf.py
+++ Python/yf.py
@@ -1,9 +1,9 @@
-#from yahoo_fin import stock_info as si
 import pandas as pd
 from sanic import Sanic
 from sanic.response import json
 import os
 from main import read_IMG,find_Amount, find_Date,find_Name
+from yahoo_fin import stock_info as si
 
 app = Sanic("Pocketmon")
 
