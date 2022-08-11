1691f3e033458cdcc0a90947e052892055874363
Andy Wong <andyw028@gmail.com>
2022-08-11 12:04:00 +0800

    ewr

--- private/script/dashboard-account.js
+++ private/script/dashboard-account.js
@@ -7,12 +7,56 @@ function eventListenerOfAccountButton() {
         console.log('1')
         loadAccountPage()
     })
+ 
 }
 function loadAccountPage() {
+
+
+
 const panel = document.querySelector('#dashboard-panel')
 panel.innerHTML = `<div id= "AccountPage" class= "row">
-<div class= "col-md-3">Username</div>
-<div class= "col-md-3">Email</div>
+<style>
+  table, th, td {
+    border:1px solid black;
+  }
+</style>
+<body>
+  
+<form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Change user name</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Change password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+
+<div id="divWrap">
+      <table id="userTable">
+      <table style="width:40%">
+        <tr>
+        <td>username</td>
+        <td>name</td>
+        <td>email</td>
+        </tr>
+        <tr>
+          <td>hellboy</td>
+          <td>roy</td>
+          <td>rr@rr.com</td>
+          </tr>
+         
+      </table>
+</div>
+
 
 `}
 
